7efe605baa23eaa21c037a52b0ea33ff35bf7f8f
Clement Rousseau <clementrousseau@Clements-MacBook-Pro.local>
2021-12-14 11:33:37 -0600

    reverting

--- start-service.sh
+++ start-service.sh
@@ -4,6 +4,7 @@ cd /home/ec2-user/app/release
 
 # Query the EC2 metadata service for this instance's region
 REGION=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq .region -r`
+echo $REGION
 # Query the EC2 metadata service for this instance's instance-id
 export INSTANCE_ID=`curl -s http://169.254.169.254/latest/meta-data/instance- id`
 # Query EC2 describeTags method and pull our the CFN Logical ID for this instance
@@ -11,5 +12,6 @@ export STACK_NAME=`aws --region $REGION ec2 describe-tags \
     --filters "Name=resource-id,Values=${INSTANCE_ID}" \
               "Name=key,Values=aws:cloudformation:stack-name" \
     | jq -r ".Tags[0].Value"`
-
+echo $STACK_NAME
+echo "now I'm going to run "
 npm run start
